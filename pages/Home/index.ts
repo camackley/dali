9c8fb465b36aa4368bb7b441d433046f4e623e11
Mackley Carmona <mackley@torre.co>
2022-09-16 23:13:47 -0500

    Centralized components in index

--- pages/Home/index.ts
+++ pages/Home/index.ts
@@ -1,9 +1,16 @@
 import Vue from "vue";
+import { UilVolume } from '@iconscout/vue-unicons';
+
+import Navbar from "@/components/Navbar/Navbar";
 
 export default Vue.extend({
+  components: {
+    Navbar,
+    IconVolume: UilVolume
+  },
   data() {
     return {
-      name: "Mackley"
+      avatarUrl: "https://firebasestorage.googleapis.com/v0/b/dali-camackley.appspot.com/o/website%20assets%2FAvatar.svg?alt=media&token=43cde588-7eff-4f55-86d9-01b2faba790b"
     };
   }
 });
--- pages/Home/index.vue
+++ pages/Home/index.vue
@@ -1,6 +1,37 @@
 <template>
-  <div>
-    Hola! {{ name }}
+  <div class="home container">
+    <Navbar/>
+
+    <div class="row">
+      <div class="col-12 col-md text-center">
+        <img
+          :class="['home__avatar', { 'home__avatar-mobile': $device.isMobileOrtablet }]"
+          :src="avatarUrl"
+          alt="Mackley photo"
+        >
+      </div>
+  
+      <div class="home__caption col-12 col-md">
+        <h2>
+          Hello there! 👀
+        </h2>
+        <p>
+          I'm
+          <span class="home__caption--name">
+            Mackley Carmona
+            <IconVolume size="18px" class="home__caption--name-icon align-top"/>
+          </span>,
+          a software engineer at
+          <a
+            href="https://torre.co"
+            class="home__caption--company"
+            target="_blank"
+          >
+            @torre</a>
+          and appasionated to never stop learning
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
