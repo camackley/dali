c7873cf03cd75c41f0144b2d2665ba15ae08d491
Mackley Carmona <mackley@torre.co>
2022-09-16 23:13:16 -0500

    Create navbar component

---
+++ components/Navbar/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="navbar container">
+    <div class="row">
+      <h1 class="navbar__logo col">
+        CaMackley
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script src='./Navbar.ts' lang='ts'></script>
+<style src='./Navbar.scss' lang='scss' scoped></style>
